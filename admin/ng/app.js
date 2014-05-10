6f983c3f7f4299dcb0dca12df9e01ffed8977e4b
Prashish Rajbhandari <prashishh@gmail.com>
2014-05-10 07:50:51 -0700

    fireabse

--- admin/ng/app.js
+++ admin/ng/app.js
@@ -1,10 +1,15 @@
 /*global angular:false, $:false, d3:false */
 'use strict';
-angular.module('votersAnalytics', ['ui.bootstrap'], function () {
-  
+angular.module('votersAnalytics', ['ui.bootstrap', 'firebase'], function () {
+
 })
   .controller('appController', function ($scope) {
     
+    var peopleRef = new Firebase("codeforindia.firebaseio.com/0");
+    
+    // Automatically syncs everywhere in realtime
+    $scope.people = $firebase(peopleRef);
+    //console.log(people);
   })
   .directive('topMenu', function () {
 	  return {
